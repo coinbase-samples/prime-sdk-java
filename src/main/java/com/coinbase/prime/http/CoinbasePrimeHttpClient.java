2aacc07ccf844dd199933d33b6e00d8f6af8865c
Nick Morgan <nick.morgan@coinbase.com>
2024-04-15 12:50:38 -0700

    move to builder pattern

--- src/main/java/com/coinbase/prime/http/CoinbasePrimeHttpClient.java
+++ src/main/java/com/coinbase/prime/http/CoinbasePrimeHttpClient.java
@@ -8,23 +8,13 @@ import java.net.http.HttpResponse;
 
 public class CoinbasePrimeHttpClient {
     HttpClient client;
-    private CoinbasePrimeCredentials credentials;
-    private String baseUrl = "https://api.prime.coinbase.com";
+    private final CoinbasePrimeCredentials credentials;
+    private final String baseUrl;
 
-    public CoinbasePrimeHttpClient(CoinbasePrimeCredentials credentials) {
-        this.credentials = credentials;
-        this.client = HttpClient.newBuilder().build();
-    }
-
-    public CoinbasePrimeHttpClient(CoinbasePrimeCredentials credentials, HttpClient client) {
-        this.credentials = credentials;
-        this.client = client;
-    }
-
-    public CoinbasePrimeHttpClient(CoinbasePrimeCredentials credentials, HttpClient client, String baseUrl) {
-        this.credentials = credentials;
-        this.client = client;
-        this.baseUrl = baseUrl;
+    private CoinbasePrimeHttpClient(Builder builder) {
+        this.credentials = builder.credentials;
+        this.client = builder.client;
+        this.baseUrl = builder.baseUrl;
     }
 
     public HttpResponse<String> get(String path) {
@@ -49,6 +39,40 @@ public class CoinbasePrimeHttpClient {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public static class Builder {
+        private final CoinbasePrimeCredentials credentials;
+        private HttpClient client;
+        private String baseUrl = "https://api.prime.coinbase.com";
+
+        public Builder(CoinbasePrimeCredentials credentials) {
+            this.credentials = credentials;
+        }
+
+        public Builder withClient(HttpClient client) {
+            this.client = client;
+            return this;
+        }
+
+        public Builder withBaseUrl(String baseUrl) {
+            this.baseUrl = baseUrl;
+            return this;
+        }
+
+        public CoinbasePrimeHttpClient build() throws IllegalStateException {
+            validate();
+            if (client == null) {
+                client = HttpClient.newBuilder().build();
+            }
+            return new CoinbasePrimeHttpClient(this);
+        }
+
+        private void validate() throws IllegalStateException {
+            if (credentials == null) {
+                throw new IllegalStateException("Credentials must be provided");
+            }
         }
     }
 }
