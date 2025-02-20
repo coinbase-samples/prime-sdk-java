acb735448a19b9c652342527ffbc9ffe30bf1042
Nick Morgan <nick.morgan@coinbase.com>
2025-02-20 11:30:44 -0800

    add transaction endpoint

---
+++ src/main/java/com/coinbase/prime/model/transactions/RPC.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2024-present Coinbase Global, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.coinbase.prime.model.transactions;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RPC {
+    @JsonProperty("skip_broadcast")
+    private boolean skipBroadcast;
+
+    private String url;
+
+    public RPC() {
+    }
+
+    public RPC(Builder builder) {
+        this.skipBroadcast = builder.skipBroadcast;
+        this.url = builder.url;
+    }
+
+    public boolean isSkipBroadcast() {
+        return skipBroadcast;
+    }
+
+    public void setSkipBroadcast(boolean skipBroadcast) {
+        this.skipBroadcast = skipBroadcast;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public static class Builder {
+        private boolean skipBroadcast;
+        private String url;
+
+        public Builder() {
+        }
+
+        public Builder skipBroadcast(boolean skipBroadcast) {
+            this.skipBroadcast = skipBroadcast;
+            return this;
+        }
+
+        public Builder url(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public RPC build() {
+            return new RPC(this);
+        }
+    }
+}
