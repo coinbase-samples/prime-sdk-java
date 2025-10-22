4d5a2d93461f0533d2d130e7e3eb06e1d153be90
Nick Morgan <nick.morgan@coinbase.com>
2025-10-22 13:47:50 -0700

    fix license header in the enum package

--- src/main/java/com/coinbase/prime/model/enums/CustodyActivityType.java
+++ src/main/java/com/coinbase/prime/model/enums/CustodyActivityType.java
@@ -1,6 +1,24 @@
 package com.coinbase.prime.model.enums;
 
 
+/*
+ * Copyright 2025-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.coinbase.prime.model.enums;
+
 public enum CustodyActivityType {
   ACTIVITY_TYPE_GOVERNANCE_VOTE,
   ACTIVITY_TYPE_INVITATION,
