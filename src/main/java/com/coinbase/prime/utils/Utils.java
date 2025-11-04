82ee7dc1f6d0212d2355e180a2fdc09c7acb91a7
Nick Morgan <nick.morgan@coinbase.com>
2025-11-04 14:35:39 -0800

    added additional utility to do parsing in the examples

--- src/main/java/com/coinbase/prime/utils/Utils.java
+++ src/main/java/com/coinbase/prime/utils/Utils.java
@@ -16,10 +16,44 @@
 
 package com.coinbase.prime.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public final class Utils {
+    private static final ObjectMapper OBJECT_MAPPER = configureObjectMapper();
+
+    private static ObjectMapper configureObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        // Include non-null properties during serialization
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        // Ignore unknown fields during deserialization
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // Read unknown enum values as null instead of failing
+        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        // Register JSR310 module for Java 8 date/time types
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+
+    /**
+     * Returns a configured ObjectMapper instance matching the configuration used by coinbase-core-java.
+     * This mapper is configured with:
+     * - NON_NULL serialization
+     * - Ignore unknown properties
+     * - Read unknown enum values as null
+     * - JSR310 JavaTimeModule for date/time support
+     *
+     * @return Configured ObjectMapper instance
+     */
+    public static ObjectMapper getObjectMapper() {
+        return OBJECT_MAPPER;
+    }
+
     public static String formatDate(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
         return sdf.format(date);
